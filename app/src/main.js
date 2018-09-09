37eed9a809f3559948dfa606553ddc2351f31941
MikeBLambert <mlamb128@gmail.com>
2018-09-09 16:07:16 -0700

    Create basic file skeleton in app

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import router from './router';
+import './main.css';
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
