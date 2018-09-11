6c3dae3bd7002adbd77a7e7519e8ad9061887426
Sophie Graham <sophiergraham1@gmail.com>
2018-09-11 11:18:32 -0700

    search displays multiple results and added user inputs

--- app/src/components/Search.vue
+++ app/src/components/Search.vue
@@ -13,12 +13,27 @@
       </div>
       <button>Search</button>
     </form>
-    <div v-if="clues">
-      <h4>Results</h4>
-      <p>Category: {{clues[0].category}}</p>
-      <p>Clue: {{clues[0].clue}}</p>
-      <p>Answer: {{clues[0].answer}}</p>
-      <p>Historic Point Value: {{clues[0].value}}</p>
+    <div 
+      v-if="clues"
+      v-for="(clue) in clues"
+      :key="clue.id">
+      <div>
+        <h4>Results</h4>
+        <p>Category: {{clues[0].category}}</p>
+        <p>Clue: {{clues[0].clue}}</p>
+        <p>Answer: {{clues[0].answer}}</p>
+        <p>Historic Point Value: {{clues[0].value}}</p>
+       <label><input type="checkbox" name="selected" value=true>Add to your game board</label><br/>
+        <label>Select point value
+          <select name="value">
+          <option value="100">100</option>
+          <option value="200">200</option>
+          <option value="300">300</option>
+          <option value="400">400</option>
+          <option value="500">500</option>
+        </select>
+        </label>
+      </div>
     </div>
   </div>
 </template>
