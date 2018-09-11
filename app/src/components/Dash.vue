2f2a94a7b5ed86d2951bb1d61303ada88bbd943f
Claire Flanagan <claireflanagan@gmail.com>
2018-09-11 10:41:00 -0700

    boards showing

--- app/src/components/Dash.vue
+++ app/src/components/Dash.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-if="user">
     <h1>Dashboard</h1>
+    <p v-for="board in boards" :key="board.id">{{ board.name }}</p>
     <router-link to="/play">Play Game</router-link>
     <router-link to="/new">Make New Game</router-link>
     <RouterView></RouterView>
@@ -8,10 +9,30 @@
 </template>
 
 <script>
+
+import { getBoards } from '../services/api';
+
 export default {
   props: {
     user: Object
+  },
+  data() {
+    return {
+      boards: null
+    };
+  },
+  created() {
+    getBoards()
+      .then(boards => {
+        console.log('boards', boards);
+        this.boards = boards;
+        console.log('this.boards', this.boards);
+      })
+      .catch(err => {
+        this.error = err;
+      });
   }
+
 };
 </script>
 
