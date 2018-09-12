2e2404a781a541cbc0b21c18677189b6619bf801
Claire Flanagan <claireflanagan@gmail.com>
2018-09-12 12:28:25 -0700

    board showing with static content

---
+++ app/src/components/GamePlay.vue
@@ -0,0 +1,118 @@
+<template>
+  <main>
+  <h1>heyyyy</h1>
+    <div class="container">
+      <div class="column" id="cat_one">
+        <div class="box">Cat 1</div>
+        <div class="box">100</div>
+        <div class="box">200</div>
+        <div class="box">300</div>
+        <div class="box">400</div>
+        <div class="box">500</div>
+      </div>
+      <div class="column" id="cat_two">
+        <div class="box">Cat 2</div>
+        <div class="box">100</div>
+        <div class="box">200</div>
+        <div class="box">300</div>
+        <div class="box">400</div>
+        <div class="box">500</div>
+      </div>
+      <div class="column" id="cat_three">
+        <div class="box">Cat 3</div>
+        <div class="box">100</div>
+        <div class="box">200</div>
+        <div class="box">300</div>
+        <div class="box">400</div>
+        <div class="box">500</div>
+      </div>
+      <div class="column" id="cat_four">
+        <div class="box">Cat 4</div>
+        <div class="box">100</div>
+        <div class="box">200</div>
+        <div class="box">300</div>
+        <div class="box">400</div>
+        <div class="box">500</div>
+      </div>
+      <div class="column" id="cat_five">
+        <div class="box">Cat 5</div>
+        <div class="box">100</div>
+        <div class="box">200</div>
+        <div class="box">300</div>
+        <div class="box">400</div>
+        <div class="box">500</div>
+      </div>
+      <div class="column" id="cat_six">
+        <div class="box">Cat 6</div>
+        <div class="box">100</div>
+        <div class="box">200</div>
+        <div class="box">300</div>
+        <div class="box">400</div>
+        <div class="box">500</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+
+.container {
+  display: grid;
+  grid-template-columns: 10vw 10vw 10vw 10vw 10vw 10vw;
+  /* grid-template-areas:
+                      'cat1 cat2 cat3 cat4 cat5 cat6'; */
+  grid-column-gap: 1vw;
+}
+
+#cat_one {
+  display: grid;
+  grid-template-rows: 5vw 5vw 5vw 5vw 5vw 5vw;
+  grid-row-gap: 1vw;
+}
+
+#cat_two {
+  display: grid;
+  grid-template-rows: 5vw 5vw 5vw 5vw 5vw 5vw;
+  grid-row-gap: 1vw;
+}
+
+#cat_three {
+  display: grid;
+  grid-template-rows: 5vw 5vw 5vw 5vw 5vw 5vw;
+  grid-row-gap: 1vw;
+}
+
+#cat_four {
+  display: grid;
+  grid-template-rows: 5vw 5vw 5vw 5vw 5vw 5vw;
+  grid-row-gap: 1vw;
+}
+
+#cat_five {
+  display: grid;
+  grid-template-rows: 5vw 5vw 5vw 5vw 5vw 5vw;
+  grid-row-gap: 1vw;
+}
+
+#cat_six {
+  display: grid;
+  grid-template-rows: 5vw 5vw 5vw 5vw 5vw 5vw;
+  grid-row-gap: 1vw;
+}
+
+.box {
+  background-color: rgb(48, 48, 162);
+  color: white;
+  font-family: 'Courier New', Courier, monospace
+}
+
+
+
+</style>
