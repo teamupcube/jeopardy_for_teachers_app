90108b1b077043a5fe824e77c2a989b6917fb466
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-13 10:07:32 -0700

    created MakeBoard.vue

---
+++ app/src/components/MakeBoard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Make Your Game Board</h2>
+    <router-link to="/board"></router-link>
+    <router-link :to="`/board/${this.board.id}`"></router-link>
+    <router-link :to="`/board/${this.category.boardId}/categories/${this.category.id}/custom-clue`"></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
