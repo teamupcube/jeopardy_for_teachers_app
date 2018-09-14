c64a709dd372580e718c02832ea63952d557862e
Sophie Graham <sophiergraham1@gmail.com>
2018-09-14 10:32:49 -0700

    added instructions page

--- app/src/components/Instructions.vue
+++ app/src/components/Instructions.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="next">
-    <h1>Instructions</h1>
+    <h1>Classroom Rules for Jeopardy:</h1>
+    <p>Teams take turns choosing and answering questions<br>
+       Scoring:<br>
+       If you get a question right, your team gains points<br>
+       If you get a question wrong, your team looses points<br>
+       If you choose not to answer, you neither loose nor gain points<br>
+       There is no double jeopardy or final jeopardy<br>
+    </p>
     <router-link :to="`/game/${this.gameId}/gameplay`">Next</router-link>
   </div>
 </template>
